4a3698c0c8de451be4843c2c81fbf6dc8ec52bbb
JWicher <jarek_wicher@wp.pl>
2020-05-09 20:20:49 +0200

    Update demo details

--- middleware/checkOrigin.js
+++
@@ -1,9 +0,0 @@
-const config = require('config');
-const validOrigin = config.get('validOrigin');
-
-module.exports = function (req, res, next) {
-    if (validOrigin !== req.header('origin'))
-        return res.status(401).send("Odmowa dostępu. Błąd podmiotu pytającego.");
-
-    else next();
-};
